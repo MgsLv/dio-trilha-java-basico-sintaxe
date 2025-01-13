acb312bb87fd6e75b894ebb8b22fe380bf007b08
Miguel Soares <mgsousa23lv@gmail.com>
2025-01-13 20:10:40 -0300

    Alteração na classe ContaTErminal: Adição de um Scanner e as variáveis que serão utilizadas ao longodo projeto

--- Estudos-Miguel/DIO/estudos/ContaBanco/src/ContaTerminal.java
+++ Estudos-Miguel/DIO/estudos/ContaBanco/src/ContaTerminal.java
@@ -1,2 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+        scn.useLocale(new Locale("pt", "BR"));
+
+        int numero;
+        double saldo;
+        String nomeCliente;
+        String agencia;
+
+        // Exibir as mensagens para o nosso usuário
+        
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+    }
 }
